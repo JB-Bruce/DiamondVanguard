b751196c2718b6df31ad9d5caac1bd26ab4d364f
Aurelien <aure.dugue@gmail.com>
2023-12-13 14:44:48 +0100

    Finalisation loot box + switch sur les panel d'equipements de chaque persos

--- Assets/Scripts/items/LootBox.cs
+++ Assets/Scripts/items/LootBox.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal.Internal;
 
 public class LootBox : MonoBehaviour
 {
@@ -23,6 +24,12 @@ public class LootBox : MonoBehaviour
     }
 
     public List<LootTable> items = new List<LootTable>();
+    private Dictionary<int, Rarity> rarityTable = new Dictionary<int, Rarity>()
+    {
+        {1, Rarity.Commun},
+        {2, Rarity.Rare},
+        {3, Rarity.Epique}
+    };
 
     [SerializeField] private lootType type;
 
@@ -49,6 +56,41 @@ public class LootBox : MonoBehaviour
         }
         else
         {
+            Debug.Log("1");
+            int rarity = 1;
+            if (randomValue <= 85f && randomValue >= 55f)
+            {
+                Debug.Log("2");
+                rarity = 2;
+            }
+            else if ( randomValue <= 100f && randomValue >= 85f)
+            {
+                Debug.Log("3");
+                rarity = 3;
+            }
+            List<LootTable> droppableItems = new List<LootTable>();
+            foreach (LootTable item in items)
+            {
+                if(item.item.rarity == rarityTable[rarity])
+                {
+                    Debug.Log("add");
+                    droppableItems.Add(item);
+                }
+            }
+            randomValue = Random.Range(0f, 100f);
+            pourcentPasse = 0;
+            float lootchance = (1f / droppableItems.Count) * 100;
+            Debug.Log(lootchance);
+            for (int i = 0; i < droppableItems.Count; i++)
+            {
+                Debug.Log("ch");
+                if (randomValue >= pourcentPasse && randomValue < (pourcentPasse + lootchance))
+                {
+                    Debug.Log("tr");
+                    return droppableItems[i].item;
+                }
+                pourcentPasse += lootchance;
+            }
 
         }
 

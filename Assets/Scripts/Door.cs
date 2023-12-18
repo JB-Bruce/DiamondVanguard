ba2ad4bdbd4e9631f1ee295cb336029da6e10b29
xinom4966 <sd.dessay@gmail.com>
2023-12-18 17:29:45 +0100

    Started working on the elevator doors

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -1,13 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Door : MonoBehaviour
 {
     [SerializeField] Animator animator;
+    [SerializeField] Outline outline;
 
     public void Open()
     {
         animator.Play("Open");
+    }
+
+    public void Select()
+    {
+        outline.enabled = true;
+    }
+
+    public void UnSelect()
+    {
+        outline.enabled = false;
     }
 }
---
+++ Assets/Scripts/ElevatorDoor.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.SearchService;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ElevatorDoor : MonoBehaviour
+{
+    [SerializeField] GameGrid gameGrid;
+    [SerializeField] private Camera cam;
+    [SerializeField] private int doorRange;
+    [SerializeField] private LayerMask itemMask;
+    [SerializeField] private string targetScene;
+    Door selectedDoor = null;
+    private void Start()
+    {
+        gameGrid = GameGrid.instance;
+    }
+
+    private void Update()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, (doorRange * gameGrid.cellSpacement), itemMask))
+        {
+            if (hit.transform.gameObject.layer == 8)
+            {
+                Door door = hit.transform.gameObject.GetComponent<Door>();
+
+                if (selectedDoor != door)
+                {
+                    if (selectedDoor != null)
+                        selectedDoor.UnSelect();
+
+                    selectedDoor = door;
+                    selectedDoor.Select();
+                }
+                if (Input.GetMouseButtonDown(0))
+                {
+                    SceneManager.LoadScene(targetScene);
+                }
+                return;
+            }
+        }
+        if (selectedDoor != null)
+        {
+            selectedDoor.UnSelect();
+            selectedDoor = null;
+        }
+    }
+}

75007c7747ba7f653d91f9d2bf6785222f6250a4
Aurelien <aure.dugue@gmail.com>
2023-12-20 15:03:51 +0100

    Amelioration formation losange

--- Assets/Scripts/Player/CharacterUIStatsUpdater.cs
+++ Assets/Scripts/Player/CharacterUIStatsUpdater.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.TextCore.Text;
 using UnityEngine.UI;
 
 public class CharacterUIStatsUpdater : MonoBehaviour

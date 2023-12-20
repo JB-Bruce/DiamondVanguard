5d89c4bbc29a83a01d1b5280352101ab98515135
Bruce-JB <118186052+JB-Bruce@users.noreply.github.com>
2023-12-20 12:15:52 +0100

    merge first part

--- Assets/Scripts/items/LootBox.cs
+++ Assets/Scripts/items/LootBox.cs
@@ -1,9 +1,5 @@
-using Microsoft.Unity.VisualStudio.Editor;
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Rendering;
-using UnityEngine.Rendering.Universal.Internal;
 
 public class LootBox : MonoBehaviour
 {

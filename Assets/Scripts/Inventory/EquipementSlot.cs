3186bebcc3df2c72fe3abdd9d288e5be3e2df571
Bruce-JB <118186052+JB-Bruce@users.noreply.github.com>
2023-12-21 17:19:23 +0100

    fixed les bugs

--- Assets/Scripts/Inventory/EquipementSlot.cs
+++ Assets/Scripts/Inventory/EquipementSlot.cs
@@ -76,7 +76,6 @@ public class EquipementSlot : ItemContainer
 
     public void SetHandItem()
     {
-
         item = startingItem;
         itemImage.sprite = startingItem.icon;
         itemImage.gameObject.SetActive(true);
